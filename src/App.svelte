5aba6e3dd308a638cf0a2b55a171415458453dd3
Jon_Snow_pt <jonsnowpt194@gmail.com>
2021-06-21 21:34:24 +0100

    CSS and PlaceHolder

--- src/App.svelte
+++ src/App.svelte
@@ -10,6 +10,7 @@
   import StarRating from "svelte-stars-rating";
   import Dropdown from "svelte-atoms/Dropdown.svelte";
   import Button from "svelte-atoms/Button.svelte";
+  import ContentLoader from "svelte-content-loader";
 
   fireStart;
 
@@ -27,6 +28,12 @@
 </script>
 
 <div class="mainDivCarousel">
+  <ContentLoader height={200} {...$$props}>
+    <circle cx="30" cy="30" r="30" />
+    <rect x="75" y="13" rx="4" ry="4" width="100" height="13" />
+    <rect x="75" y="37" rx="4" ry="4" width="50" height="8" />
+    <rect x="0" y="70" rx="5" ry="5" width="400" height="400" />
+  </ContentLoader>
   {#await sportsBooksResult then items}
     <Carousel
       dots={false}
@@ -69,14 +76,14 @@
                       />
                     </a>
                   </div>
-                  <div>{item.bonus_description}</div>
-                  <div>{item.bonus_code}</div>
+                  <div class="bonusDescription">{item.bonus_description}</div>
+                  <div class="bonusCode">{item.bonus_code}</div>
                   <div class="btnDiv">
                     <a href={item.register_link}>
                       <button class="btnWebRegister">Register</button>
                     </a>
                   </div>
-                  <div>{item.information}</div>
+                  <div class="bonusInfo">{item.information}</div>
                 </div>
               </Dropdown>
             </div>
@@ -111,9 +118,9 @@
   .content {
     padding: 8px;
     color: white;
-    font-family: Roboto;
+    font-family: "Roboto", sans-serif;
     font-style: normal;
-    font-weight: normal;
+    font-weight: 500px;
     font-size: 16px;
     text-align: center;
   }
@@ -151,11 +158,11 @@
 
   .titleSports {
     position: relative;
+    font-weight: 500;
     margin-top: -53px;
     width: 58px;
     height: 27px;
     left: 108px;
-    font-weight: 500;
     color: #fff;
   }
 
@@ -204,9 +211,35 @@
   }
 
   .btnWebRegister {
-    width: 200px;
-    margin-top: 0px;
-    margin-left: 0px;
+    width: 260px;
+    height: 44px;
+
+    /* Property 1=web */
+    left: calc(50% - 151px / 2 - 167px);
+    top: 20px;
+    cursor: pointer;
+    /* accent/normal */
+    background: #f5620f;
+    /* shadow/orange */
+    box-shadow: 0px 3px 8px rgba(212, 84, 12, 0.32);
+    border-radius: 8px;
+    border: 0px solid;
+
+    /* Content Button */
+    left: 52px;
+    top: calc(50% - 21px / 2 + 0.5px);
+    /* nav/14px */
+    font-family: "Roboto", sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 150%;
+    /* black/white */
+    color: #ffffff;
+  }
+
+  .btnWebRegister:hover {
+    background-color: #f77c42;
   }
 
   .mainDivStyle {
@@ -230,10 +263,59 @@
     margin-top: -30px;
   }
 
+  .bonusDescription {
+    margin-top: 30px;
+    /* body/16px */
+
+    font-family: "Roboto", sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 150%;
+    /* identical to box height, or 24px */
+
+    text-align: center;
+
+    /* black/white */
+
+    color: #ffffff;
+  }
+
+  .bonusCode {
+    margin-top: 20px;
+    font-family: "Roboto", sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 150%;
+    /* identical to box height, or 21px */
+
+    text-align: center;
+  }
+
+  .bonusInfo {
+    margin-top: 30px;
+    margin-bottom: 30px;
+    /* body/12px */
+
+    font-family: "Roboto", sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 150%;
+    /* or 18px */
+
+    text-align: center;
+
+    /* black/gray */
+
+    color: #cccccc;
+  }
+
   .btnDiv {
     width: 100%;
     text-align: center;
-    margin-top: 200px;
+    margin-top: 30px;
   }
 
   .mainDivCarousel {
